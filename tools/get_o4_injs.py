1f73d23c2285ac12593d12f4e2aba3ae28776fb5
sharanbngr <sharanbngr@gmail.com>
2024-05-09 10:58:58 -0500

    spin jacobians for mixture models

--- tools/get_o4_injs.py
+++ tools/get_o4_injs.py
@@ -14,7 +14,7 @@ def detected_injections(snr, snr_threshold):
 
 def get_o1o2o3o4_injs(snr_threshold):
 
-    injs = h5py.File('/home/rp.o4/offline-injections/mixtures/T2400110-v2/rpo1234-cartesian_spins-semianalytic_o1_o2_o4a-real_o3.hdf', 'r')
+    injs = h5py.File('./rpo1234-cartesian_spins-semianalytic_o1_o2_o4a-real_o3.hdf', 'r')
     savefile = './o1o2o3o4_prelim_injs_snr-' + str(snr_threshold) + '.pkl'
 
 
@@ -59,8 +59,11 @@ def get_o1o2o3o4_injs(snr_threshold):
     o1o2o3o4_injs['cos_tilt_1'] = a1z / o1o2o3o4_injs['a_1']
     o1o2o3o4_injs['cos_tilt_2'] = a2z / o1o2o3o4_injs['a_2']
 
-    jacobian_spin1 = 2 * np.pi * o1o2o3o4_injs['a_1']**2
-    jacobian_spin2 = 2 * np.pi * o1o2o3o4_injs['a_2']**2
+    jacobian_spin1 = np.sqrt(1 - o1o2o3o4_injs['cos_tilt_1']**2)
+    jacobian_spin2 = np.sqrt(1 - o1o2o3o4_injs['cos_tilt_2']**2)
+
+    #jacobian_spin1 = 2 * np.pi * o1o2o3o4_injs['a_1']**2
+    #jacobian_spin2 = 2 * np.pi * o1o2o3o4_injs['a_2']**2
     o1o2o3o4_injs['prior'] *= jacobian_spin1 * jacobian_spin2 * o1o2o3o4_injs['mass_1']
 
     for key in o1o2o3o4_injs.keys():
