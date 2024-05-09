182f3324d561ccafb482ad8e35b2be1a1ba5976a
Sharan <sharan.banagiri@ligo.org>
2024-05-09 08:56:30 -0700

    update redshift code to include astropy cosmo.z2DL

--- tools/cbcflow_pe_pull.py
+++ tools/cbcflow_pe_pull.py
@@ -1,9 +1,11 @@
 import numpy as np
 import cbcflow
-import matplotlib.pyplot as plt
 import h5py
+import pickle
 import pandas as pd
 from cosmology import PLANCK_2015_Cosmology as cosmo
+from astropy.cosmology import Planck15
+import astropy.units as u
 
 def construct_prior_dl_to_z(data):
     z_max = 1.9
@@ -12,43 +14,44 @@ def construct_prior_dl_to_z(data):
         z_max = max(data["redshift"])
 
     zs = np.linspace(0, z_max * 1.01, 1000)
-    
+
     ## Convert to dl
     dl = cosmo.z2DL(zs) / 1e3
     p_z = dl**2 * (dl / (1 + zs) + (1 + zs) * cosmo.dDcdz(zs, mpc=True) / 1e3)
     p_z /= np.trapz(p_z, zs)
-    
-    
+
+
     data["prior"] = np.interp(np.array(data["redshift"]), zs, p_z) * data["mass_1"] * (1 + data["redshift"]) ** 2
-    
+
     # For spins
     data["prior"] /= 4
-    
+
     return data
 
 
 
 def construct_O4a_prelim_prior(data):
 
-    z_max = 1.9
+    z_max = 2.3
 
     if max(data["redshift"]) > z_max:
         z_max = max(data["redshift"])
 
     zs = np.linspace(0, z_max * 1.01, 1000)
 
-    ## calc Dc
-    dc = cosmo.z2DL(zs) / 1e3 / (1 + zs)
+    p_z = Planck15.differential_comoving_volume(zs).to(u.Gpc**3/u.sr) / (1 + zs)
 
-    p_z = dc**2 * cosmo.dDcdz(zs, mpc=True) / 1e3  / (1 + zs)
+    p_z = p_z.value
+
     p_z /= np.trapz(p_z, zs)
 
     p_z =  np.interp(np.array(data["redshift"]), zs, p_z)
 
-    ## calc the symmetric mass ratio
-    eta = data['mass_ratio'] / (1 + data['mass_ratio'])**2
 
-    data["prior"] = p_z * (1 + data["redshift"]) * (1 + data['mass_ratio']) * eta**(3/5)
+    ## calc the symmetric mass ratio
+    #eta = data['mass_ratio'] / (1 + data['mass_ratio'])**2
+
+    data["prior"] = p_z * data["mass_1"] * (1 + data['redshift'])**2 / 4
 
     return data
 
@@ -60,17 +63,26 @@ library = cbcflow.database.LocalLibraryDatabase('/home/cbc/cbcflow/O4a/cbc-workf
 o4pe = {}
 
 for sevent in library.superevents_in_library:
+
     #print('extracting info for ' + sevent)
+    most_likely_bns = ['S231030av', 'S230918aq', 'S230810af', 'S230524x']
+    msot_likely_nsbh = ['S230830b', 'S230715bw', 'S230627c', 'S230529ay', 'S230518h']
+
     seventdata = cbcflow.get_superevent(sevent)
 
-    isretracted = False
+    dontpullpe = False
     if len(seventdata["Info"]["Notes"]) !=0:
         for note in seventdata["Info"]["Notes"]:
             if "retracted:" in note.lower():
-                isretracted = True
+                dontpullpe = True
+                print(sevent + ' has been retracted')
+
+    if sevent in most_likely_bns or sevent in msot_likely_nsbh:
+        dontpullpe = True
+        print(sevent + ' most likely a BNS or an NSBH')
 
 
-    if not isretracted:
+    if not dontpullpe:
 
         far = 10
         for uevent in seventdata['GraceDB']['Events']:
@@ -84,20 +96,25 @@ for sevent in library.superevents_in_library:
         else:
             try:
                 illustrative = seventdata['ParameterEstimation']['IllustrativeResult']
+                illustrative_exists = True
+            except:
+                illustrative_exists = False
+                print('no illustrative PE for ' + sevent)
+
+            if illustrative_exists:
                 for pe in seventdata['ParameterEstimation']['Results']:
                     if pe['UID'].casefold() == illustrative.casefold():
-                        illustrative_exists = True
 
                         pe_data = h5py.File(pe['ResultFile']['Path'][4:], 'r')
 
 
                         df = {
-                            'mass_1': pe_data['posterior']['mass_1_source'][:], 
+                            'mass_1': pe_data['posterior']['mass_1_source'][:],
                             'mass_2': pe_data['posterior']['mass_2_source'][:],
                             'mass_ratio': pe_data['posterior']['mass_ratio'][:],
-                            'redshift': pe_data['posterior']['redshift'][:],            
+                            'redshift': pe_data['posterior']['redshift'][:],
                             'a_1': pe_data['posterior']['a_1'][:],
-                            'a_2': pe_data['posterior']['a_2'][:], 
+                            'a_2': pe_data['posterior']['a_2'][:],
                             'cos_tilt_1': pe_data['posterior']['cos_tilt_1'][:],
                             'cos_tilt_2': pe_data['posterior']['cos_tilt_2'][:],
                             'chi_eff': pe_data['posterior']['chi_eff'][:],
@@ -109,12 +126,12 @@ for sevent in library.superevents_in_library:
                         df = pd.DataFrame(data=df)
 
                         o4pe[sevent] = df
-            except:
-                print('no illustrative PE for ' + sevent)
 
-    else:
-        print(sevent + ' has been retracted')
+
+
 
 print('PE obtained for ' + str(len(o4pe.keys())) + ' triggers')
 
-import pdb; pdb.set_trace()
+with open('o4a_prelim_pe.pkl', 'wb') as f:
+    pickle.dump(o4pe, f)
+
